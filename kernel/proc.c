63f9f83049a873167c7cd8cf023209bcb8db329f
vaquitson <vaquitson@gmail.com>
2024-10-14 21:53:59 -0300

    final : (

--- kernel/proc.c
+++ kernel/proc.c
@@ -120,7 +120,7 @@ allocproc(void)
   for(p_ = proc; p_ < &proc[NPROC]; p_++) {
     acquire(&p_->lock);
 
-    if (p_->state == RUNNABLE || p_->state == RUNNING){
+    if (p_->state == RUNNABLE || p_->state == SLEEPING || p_->state == RUNNING){
       // actualizar prioridades de procesos RUNNABLE cada vez que entra
       // un nuevo proceso
       p_->priority += p_->boost;
@@ -479,36 +479,49 @@ scheduler(void)
   struct proc *p;
   struct cpu *c = mycpu();
   c->proc = 0;
+
+  int found;
+  int foundMatchingPriority;
+  int i;
   for(;;){
     // The most recent process to run may have had interrupts
     // turned off; enable them to avoid a deadlock if all
     // processes are waiting.
     intr_on();
 
-    int found = 0;
-    int foundMatchingPriority = 0;
+    found = 0;
 
-    for(p = proc; p < &proc[NPROC]; p++) { 
-      acquire(&p->lock);
-      if(p->state == RUNNABLE) {
-        if (curPrioToRun == p->priority){
-          // Switch to chosen process.  It is the process's job
-          // to release its lock and then reacquire it
-          // before jumping back to us.
-          p->state = RUNNING;
-          c->proc = p;
+    for (i = 0; i < 10; i++){
+      foundMatchingPriority = 0;
+      for(p = proc; p < &proc[NPROC]; p++){ 
+        acquire(&p->lock);
+        if(p->state == RUNNABLE) {
+          if (p->priority == i){
+            // Switch to chosen process.  It is the process's job
+            // to release its lock and then reacquire it
+            // before jumping back to us.
+            p->state = RUNNING;
+            c->proc = p;
 
-          printf("proces: %d prio: %d\n", p->pid, p->priority);
-          swtch(&c->context, &p->context);
-          // Process is done running for now.
-          // It should have changed its p->state before coming back.
-          c->proc = 0;
+            printf("proces: %d prio: %d\n", p->pid, p->priority);
+            swtch(&c->context, &p->context);
+            // Process is done running for now.
+            // It should have changed its p->state before coming back.
+            c->proc = 0;
 
-          foundMatchingPriority = 1;
-        }
-        found = 1;
-      } 
-      release(&p->lock);
+            foundMatchingPriority = 1;
+          }
+          found = 1;
+        } else if (p->priority < i && p->state == RUNNABLE){
+          i = 0;
+          p = proc;
+        } 
+        release(&p->lock);
+      }
+      if (foundMatchingPriority == 1){
+        // si encontramos algun proceso en esta prioridad que sea RUNNABLE
+        i--;
+      }
     }
 
     if(found == 0) {
@@ -517,15 +530,6 @@ scheduler(void)
       asm volatile("wfi");
     }
     
-    // si no encontramos nada con la prioridad actual, aumentamos la prioridad que buscamos
-    if (foundMatchingPriority == 0){
-
-      curPrioToRun++;
-      // si la prioridad es mayor a la maxima, volvemos a 0
-      if (curPrioToRun > 9){
-        curPrioToRun = 0;
-      }
-    }
 
   }
 }
--- user/priority_test.c
+++ user/priority_test.c
@@ -13,10 +13,6 @@ int main(void){
       int procesPid = getpid();
       printf("process created: %d\n", procesPid);
       sleep(3);
-      for (int j = 0; j < 1000000*i; j++){
-        void *ptr = malloc(30); //perder tiempo en cpu
-        free(ptr);
-      }
       printf("process finish: %d\n", procesPid);
       break;
 
